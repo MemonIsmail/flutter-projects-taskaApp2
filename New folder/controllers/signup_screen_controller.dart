67fd10ef0272071cd5d150a08fac0bc90064544c
Memon Ismail <115149149+MemonIsmail@users.noreply.github.com>
2026-04-08 18:12:44 +0500

    Delete New folder directory

--- New folder/controllers/signup_screen_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get/get.dart';
-import 'package:taska_app/model/constants.dart';
-
-class SignupScreenController extends GetxController{
-  void onSigninPressed(){
-    Get.offNamed(kLoginScreenRoute);
-  }
-}
--- New folder/lib/model/constants.dart
+++
@@ -1,5 +0,0 @@
-const String kSplashScreenRoute = '/SPLASH_SCREEN_ROUTE';
-const String kLoginScreenRoute = '/LOGIN_SCREEN_ROUTE';
-const String kSignupScreenRoute = '/SIGNUP_SCREEN_ROUTE';
-const String kDashboardScreenRoute = '/DASHBOARD_SCREEN_ROUTE';
-const String kTaskScreenRoute = '/TASK_SCREEN_ROUTE';
