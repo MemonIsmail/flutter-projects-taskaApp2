6ef6a5e0ca8d6ebab082cc9edd09ab2068d3dc3c
Memon Ismail <115149149+MemonIsmail@users.noreply.github.com>
2026-04-22 18:18:54 +0500

    Delete New folder 2 directory

--- New folder 2/lib/database/services/web/urls.dart
+++
@@ -1,4 +0,0 @@
-const String kBaseURL = 'https://dummyjson.com/';
-const String kLoginURL = '${kBaseURL}auth/login';
-const String kGetUsersURL = '${kBaseURL}users';
-const String kGetTasksURL = '${kBaseURL}todos/?limit=100';
