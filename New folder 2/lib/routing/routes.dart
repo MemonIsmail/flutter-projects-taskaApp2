9a9236226541139c95c8dc02c285bfcdf23dc295
Memon Ismail <115149149+MemonIsmail@users.noreply.github.com>
2026-04-23 07:57:58 +0500

    Delete New folder 2/lib directory

--- New folder 2/lib/routing/routes.dart
+++
@@ -1,6 +0,0 @@
-const String kSplashScreenRoute = '/SPLASH_SCREEN_ROUTE';
-const String kLoginScreenRoute = '/LOGIN_SCREEN_ROUTE';
-const String kSignupScreenRoute = '/SIGNUP_SCREEN_ROUTE';
-const String kDashboardScreenRoute = '/DASHBOARD_SCREEN_ROUTE';
-const String kCreateTaskRoute = '/CREATE_TASK_ROUTE';
-const String kTaskDetailsRoute = '/TASK_DETAILS_ROUTE';
